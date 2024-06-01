33b88af9d7248f9e9318168af9b79cd8e2b7880b
arvee0220 <rowellverdejo@gmail.com>
2024-06-01 19:47:13 -0400

    Update Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,10 +1,16 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants/constants";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = () => {
 	return (
 		<div className="flex flex-row flex-wrap justify-center gap-10">
+			<motion.div variants={textVariant}>
+				<h2 className={styles.sectionHeadText}>Tech Stack</h2>
+			</motion.div>
 			{technologies.map((tech) => (
 				<div className="w-28 h-28" key={tech.name}>
 					<BallCanvas icon={tech.icon} />
