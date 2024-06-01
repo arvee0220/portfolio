d3b904c45d8a635b8f46b6d089b192ca3e958f0d
arvee0220 <rowellverdejo@gmail.com>
2024-06-01 19:54:59 -0400

    Update Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,16 +7,18 @@ import { styles } from "../styles";
 
 const Tech = () => {
 	return (
-		<div className="flex flex-row flex-wrap justify-center gap-10">
+		<>
 			<motion.div variants={textVariant}>
 				<h2 className={styles.sectionHeadText}>Tech Stack</h2>
 			</motion.div>
-			{technologies.map((tech) => (
-				<div className="w-28 h-28" key={tech.name}>
-					<BallCanvas icon={tech.icon} />
-				</div>
-			))}
-		</div>
+			<div className="flex flex-wrap justify-center gap-10">
+				{technologies.map((tech) => (
+					<div className="w-28 h-28" key={tech.name}>
+						<BallCanvas icon={tech.icon} />
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
